25c738cd27a87c9aa75f54832034451ff1a62f76
konundrum <nnrbgb@gmail.com>
2016-03-18 00:21:15 -0700

    Fix DataView order and consolidate files.

--- pkas/data.py
+++ pkas/data.py
@@ -1,15 +1,97 @@
+from collections import defaultdict
 from kivy.event import EventDispatcher
 from kivy.properties import BooleanProperty, ObjectProperty, StringProperty
-from .factory import factory
 
 
+QUEUE_LEN = 10
+
+
+
+
+class Factory(object):
+
+  # Singleton reference
+  _inst = None
+
+
+
+  def __init__(self):
+    super().__init__()
+    self._ctors = {}
+    self._recycled = defaultdict(list)
+    self._queue_lengths = {}
+
+
+
+  def __new__(cls):
+    if not cls._inst:
+      cls._inst = super().__new__(cls)
+
+    return cls._inst
+
+
+
+  def make(self, class_name, *args, **kwargs):
+    try:
+      Ctor = self._ctors[class_name]
+    except KeyError:
+      raise Exception('factory.make:', name,'not specified.')
+
+    try:
+      return self._recycled[class_name].pop().init(*args, **kwargs)
+    except IndexError:
+      pass
+
+    return Ctor(*args, **kwargs)
+
+
+
+  def recycle(self, obj):
+    class_name = obj.__class__.__name__
+    obj_queue = self._recycled[class_name]
+    max_queue = self._queue_lengths[class_name]
+
+    if len(obj_queue) < max_queue:
+      obj_queue.append(obj.reset())
+      
+
+
+  def set_queue_length(self, class_name, length):
+    self._queue_lengths[class_name] = length
+    recycled = self._recycled[class_name]
+    if len(recycled) > length:
+      self._recycled[class_name] = recycled[:length]
+
+
+    
+  def specify(self, Ctor, length):
+    self._ctors[Ctor.__name__] = Ctor
+    self._queue_lengths[Ctor.__name__] = length
+
+
+
+
+factory = Factory()
+
+
+def specify(Ctor, length=QUEUE_LEN):
+  factory.specify(Ctor, length)
+  return Ctor
+
+
+
+
+@specify
 class DataModel(EventDispatcher):
+
   is_selected = BooleanProperty(False)
 
 
   def __init__(self, **kwargs):
-    super().__init__(self, **kwargs)
     self.register_event_type('on_change')
+    self._id = None
+    super().__init__(**kwargs)
+
 
 
   def __setattr__(self, k, v):
@@ -18,20 +100,25 @@ class DataModel(EventDispatcher):
     super().__setattr__(k, v)
 
 
+
+  def __repr__(self):
+    return self._id if self._id else self.__class__.__name__
+
+
+
   def to_obj(self):
     obj = {}
     obj['_class'] = self.__class__.__name__
     obj['_id'] = self._id
-
-
     for key in self.properties():
         obj[key] = repr(self[key])
-
     return obj
 
 
+
   def on_change(self, v):
-    pass
+    return
+
 
 
   def apply(self, **kwargs):
@@ -54,65 +141,55 @@ class DataModel(EventDispatcher):
 
 
 
+@specify
+class DataList(DataModel):
 
-
-class DataList(EventDispatcher, list):
   
-  def __init__(self, **kwargs):
+  def __init__(self, *args, **kwargs):
     super().__init__(**kwargs)
+    self._list = list(*args)
     self.register_event_type('on_insert')
     self.register_event_type('on_refresh')
     self.register_event_type('on_remove')
     self.register_event_type('on_set')
     self.register_event_type('on_swap')
-  
-
-  def on_insert(self, i, x):
-    pass
-  def on_refresh(self):
-    pass
-  def on_remove(self, x):
-    pass
-  def on_set(self, i, v):
-    pass
-  def on_swap(self, a, b):
-    pass
 
 
   def __delitem__(self, i):
-    super().__delitem__(i)
-    self.dispatch('remove', i)
-    
+    self._list.__delitem__(i)
+    i =  len(self._list) - i
+    self.dispatch('on_remove', i)
+   
+
+  def __iter__(self):
+    return iter(self._list)
+
+
+  def __len__(self):
+    return len(self._list)
+
 
   def __setitem__(self, i, v):
-    super().__setitem__(i, v)
-    self.dispatch('set', i, v)
+    self._list.__setitem__(i, v)
+    i =  len(self._list) - 1 - i
+    self.dispatch('on_set', i, v)
 
 
   def __repr__(self):
-    return '[' + ''.join('{},'.format(model._id) for model in self) + ']'
+    return '[' + ''.join('{}, '.format(repr(model)) for model in self) + ']'
+
+
+
+  def append(self, x):
+    self._list.append(x)
+    self.dispatch('on_insert', len(self._list) - 2, x)
+
 
 
   def clear(self):
-    super().clear()
-    self.dispatch('refresh', self)
+    self._list.clear()
+    self.dispatch('on_refresh')
 
-  def pop(self, i=None):
-    super().pop(i)
-    self.dispatch('remove', i)
-
-
-  def refresh(self):
-    self.dispatch('refresh', self)
-
-
-  def swap(self, a, b):
-    self[a], self[b] = self[b], self[a]
-    self.dispatch('swap', a, b)
-
-  def append(self, x):
-    super().append(x)
-    self.dispatch('insert', len(self) - 2, x)
 
   
   def extend(self, L):
@@ -120,51 +197,128 @@ class DataList(EventDispatcher, list):
       self.append(x)
 
 
+
   def insert(self, i, x):
-    super().insert(i, x)
-    self.dispatch('insert', i, x)
+    self._list.insert(i, x)
+    self.dispatch('on_insert', i, x)
+
+
+
+  def pop(self, i=None):
+    self._list.pop(i)
+    i =  len(self._list) - i
+    self.dispatch('on_remove', i)
+
+
+
+  def refresh(self):
+    self.dispatch('on_refresh')
+
 
 
   def remove(self, x):
-    i = self.index(x)
-    super().remove(x)
-    self.dispatch('remove', i)
+    _list = self._list
+    i =  len(_list) - 1 - _list.index(x)
+    _list.remove(x)
+    self.dispatch('on_remove', i)
 
-
-  def sort(self, cmp=None, key=None, reverse=False):
-    super().sort(cmp, key, reverse)
-    self.dispatch('refresh', self)
 
 
   def reverse(self):
-    super().reverse()
-    self.dispatch('refresh', self)
+    self._list.reverse()
+    self.dispatch('on_refresh')
 
 
+
+  def sort(self, cmp=None, key=None, reverse=False):
+    self._list.sort(cmp, key, reverse)
+    self.dispatch('on_refresh')
+
+
+
+  def swap(self, a, b):
+    self[a], self[b] = self[b], self[a]
+    _len =  len(self._list) - 1
+    a, b = (_len - a), (_len - b)
+    self.dispatch('on_swap', a, b)
+
+
+
+  def on_insert(self, i, x):
+    pass
+  
+  def on_refresh(self):
+    pass
+  
+  def on_remove(self, i):
+    pass
+  
+  def on_set(self, i, v):
+    pass
+  
+  def on_swap(self, a, b):
+    pass
 
 
 
 
 from random import random
 
-class DataContext(EventDispatcher):
 
-  is_selected = BooleanProperty(False)
+@specify
+class DataContext(DataModel):
+
   name = StringProperty('')
-  store = ObjectProperty(None)
+  store = ObjectProperty(None, allownone=True)
 
 
-  def __init__(self):
-    super().__init__()
+  def __init__(self, **kwargs):
+    super().__init__(**kwargs)
     self.factory = factory
     self._data = {}
     self._uids = {}
     self.clear_changes()
 
 
+  def __getitem__(self, _id):
+    return self._data[_id]
+
+
+
+  def _get_id(self):
+    for i in range(2):
+      _id = str(random())
+      try:
+        self[_id]
+      except KeyError:
+        return _id
+    
+    raise Exception('couldnt create _id after 2 tries.')
+
+
+
   def clear_changes(self):
     self.changed = {}
     self.deleted = {}
+
+
+
+  def delete(self, _id):
+    model = self.deleted[_id] = self._data[_id]
+    del self._data[_id]
+
+    model.unbind_uid('on_change', self._uids[_id])
+    del self._uids[_id]
+
+    try:
+      del self.changed[_id]
+    except KeyError:
+      pass
+
+
+
+  def get(self, _id):
+    return self._data[_id]
 
 
 
@@ -183,6 +337,22 @@ class DataContext(EventDispatcher):
 
 
 
+  def put(self, model):
+    try:
+      _id = model._id
+    except AttributeError:
+      model._id = _id = self._get_id()
+
+    self._uids[_id] = model.fbind('on_change', self.register_change)
+    self._data[_id] = model
+    self.changed[_id] = model
+
+
+
+  def register_change(self, model):
+    self.changed[model._id]
+
+
 
   def save(self):
     delete = self.store.delete
@@ -198,55 +368,4 @@ class DataContext(EventDispatcher):
         put(_id, **model.to_obj())
 
     self.clear_changes()
-
-
-
-  def delete(self, _id):
-    model = self.deleted[_id] = self._data[_id]
-    del self._data[_id]
-
-    model.unbind_uid('on_change', self._uids[_id])
-    del self._uids[_id]
-
-    try:
-      del self.changed[_id]
-    except KeyError:
-      pass
-
-
-
-
-  def _get_id(self):
-    for i in range(2):
-      _id = str(random())
-      try:
-        self[_id]
-      except KeyError:
-        return _id
-    
-    raise Exception('couldnt create _id after 2 tries.')
-
-
-
-  def register_change(self, model):
-    self.changed[model._id]
-
-
-  def put(self, model):
-    try:
-      _id = model._id
-    except AttributeError:
-      model._id = _id = self._get_id()
-
-    self._uids[_id] = model.fbind('on_change', self.register_change)
-    self._data[_id] = model
-    self.changed[_id] = model
-
-
-  def get(self, _id):
-    return self._data[_id]
-
-  def __getitem__(self, _id):
-    return self._data[_id]
-
 
--- pkas/ui.py
+++ pkas/ui.py
@@ -1,7 +1,10 @@
-from kivy.lang import Builder
+from .data import factory
 from kivy.event import EventDispatcher
-from kivy.properties import AliasProperty, BooleanProperty
+from kivy.properties import AliasProperty, BooleanProperty, ObjectProperty
+from kivy.uix.widget import Widget
+
 from os.path import join
+from kivy.lang import Builder
 
 
 def load_kv(*args):
@@ -9,8 +12,11 @@ def load_kv(*args):
 
 
 
+
 class Interactive(EventDispatcher):
+
   is_active = BooleanProperty(False)
+  
   
   def __init__(self, **kwargs):
     super().__init__(**kwargs)
@@ -25,29 +31,145 @@ class Interactive(EventDispatcher):
 
 
 
+class DataWidget(Widget):
 
-class Selector(EventDispatcher):
+  is_selected = BooleanProperty(False)
+  model = ObjectProperty(None, rebind=True, allownone=True)
 
-  def _get_selected(self):
+
+  def __init__(self, **kwargs):
+    super().__init__(**kwargs)
+
+
+
+  def reset(self):
+    self.is_selected = False
+    self.model = self.property('model').defaultvalue
+    return self
+
+
+
+
+class DataView(Widget):
+
+  def _get_data(self):
+    return self._data
+
+  def _set_data(self, data):
+    if self._data:
+      self.unbind_data()
+    self._data = data
+    self.bind_data()
+    self.on_refresh(data)
+
+
+  data = AliasProperty(_get_data, _set_data, bind=[])
+
+
+  def __init__(self, **kwargs):
+    self._data = None
+    super().__init__(**kwargs)
+    self.factory = factory
+    self._bound_uids = []
+    self.__class__.data_widget = self.__class__.data_widget.__name__
+
+
+    
+  def __del__(self):
+    try:
+      self.unbind_data()
+    except IndexError:
+      pass
+
+
+
+  def bind_data(self):
+    append_uid = self._bound_uids.append
+    fbind = self.data.fbind
+    append_uid(fbind('on_insert', self.on_insert))
+    append_uid(fbind('on_refresh', self.on_refresh))
+    append_uid(fbind('on_remove', self.on_remove))
+    append_uid(fbind('on_set', self.on_set))
+    append_uid(fbind('on_swap', self.on_swap))
+
+
+
+  def unbind_data(self):
+    uids = self._bound_uids
+    unbind_uid = self.data.unbind_uid
+    unbind_uid('on_swap', uids.pop())
+    unbind_uid('on_set', uids.pop())
+    unbind_uid('on_remove', uids.pop())
+    unbind_uid('on_refresh', uids.pop())
+    unbind_uid('on_insert', uids.pop())
+
+
+
+  def on_insert(self, data, i, model):
+    widget = self.factory.make(self.data_widget, model=model)
+    self.add_widget(widget, i)
+
+
+
+  def on_refresh(self, data):
+    widget_class = self.data_widget
+    make, recycle = self.factory.make, self.factory.recycle
+    add, remove = self.add_widget, self.remove_widget
+
+    for widget in self.children:
+      remove(widget)
+      recycle(widget)
+
+    for model in data:
+      add(make(widget_class, model=model))
+
+
+
+  def on_remove(self, data, i):
+    widget = self.children[i]
+    self.remove_widget(widget)
+    self.factory.recycle(widget)
+
+
+
+  def on_set(self, data, i, model):
+    widget = self.factory.make(self.data_widget, model=model)
+    old_widget = self.children[i]
+    self.children[i] = widget
+    self.factory.recycle(old_widget)
+
+
+
+  def on_swap(self, data, a, b):
+    children = self.children
+    children[a], children[b] = children[b], children[a]
+
+
+
+
+class Walker(EventDispatcher):
+  
+  def _get_position(self):
     try:
       return self._widgets[self._index]
     except IndexError:
       return None
 
-
-  def _set_selected(self, widget):
+  def _set_position(self, widget):
     index = self._widgets.index(widget)
     if index != self._index:
       self._index = index
-      return True
 
-  selected = AliasProperty(_get_selected, _set_selected, bind=[])
+
+  position = AliasProperty(_get_position, _set_position, bind=[])
   
+
 
   def __init__(self, widgets, index = 0):
     super().__init__()
     self._widgets = widgets
     self._index = index
+
 
 
   def increment(self):
@@ -56,8 +178,10 @@ class Selector(EventDispatcher):
     return self._widgets[self._index]
 
 
+
   def decrement(self):
     if self._index > 0:
       self._index -= 1
     return self._widgets[self._index]
 
+    
